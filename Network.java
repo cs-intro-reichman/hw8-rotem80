5e7cc2f4f2c03e8e25f8e619274013ff7d5bae80
rotem80 <rrolev18@gmail.com>
2025-01-09 13:01:42 +0200

    NET 5

--- Network.java
+++ Network.java
@@ -115,16 +115,16 @@ public class Network {
         return count;
     }
 
-    /** Returns a textual description of all the users in this network, and who they follow. */
-    public String toString() {
-        StringBuilder sb = new StringBuilder("Network:\n");
-        for (int i = 0; i < userCount; i++) {
-            sb.append(users[i].getName()).append(" -> ");
-            for (int j = 0; j < users[i].getfCount(); j++) {
-                sb.append(users[i].getfFollows()[j]).append(" ");
-            }
-            sb.append("\n");
+/** Returns a textual description of all the users in this network, and who they follow. */
+public String toString() {
+    StringBuilder sb = new StringBuilder("Network:\n");
+    for (int i = 0; i < userCount; i++) {
+        sb.append(users[i].getName()).append(" -> ");
+        for (int j = 0; j < users[i].getfCount(); j++) {
+            sb.append(users[i].getfFollows()[j]).append(" ");
         }
-        return sb.toString().trim();
+        sb.append("\n");
     }
+    return sb.toString();
+}
 }
